e821d01b09bd0cee1ab73731d5f5c0ee766a6719
Матвей <95907663+Matvey2022@users.noreply.github.com>
2022-04-29 18:31:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+from email import message
 from setuptools import Command
 import telebot
 bot = telebot.TeleBot ( "5383758988:AAG-XTVK3fzCrQ0GE2kTE76vMKllg0dvc_Q", parse_mode=None )
-@bot.massage_handler(commands = ['start'])
-    def send_welcom(message):
-        bot.reply_to(message, '''hi
-    
-        ''')
+@bot.massage_handler(command = ['start'])
+def  send_welcom(message):
+        bot.reply_to(message,'''hi''')
