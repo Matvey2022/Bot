cf20275ab3adae647b4fc8555ab4cbe697c09257
Матвей <95907663+Matvey2022@users.noreply.github.com>
2022-04-29 18:35:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from email import message
 from setuptools import Command
 import telebot
 bot = telebot.TeleBot ( "5383758988:AAG-XTVK3fzCrQ0GE2kTE76vMKllg0dvc_Q", parse_mode=None )
-@bot.massage_handler(command = ['start'])
+@bot.message_handler(command = ['start'])
 def  send_welcom(message):
         bot.reply_to(message,'''hi''')
+
+
+
+
+
+
+bot.infinity_polling()    
